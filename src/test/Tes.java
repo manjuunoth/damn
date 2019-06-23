2aabb550b740ea617bb0d6ac8264387153f35200
manjunath <manjuunothda@gmail.com>
2019-06-23 17:16:26 +0530

    jj

--- src/test/Tes.java
+++ src/test/Tes.java
@@ -24,8 +24,10 @@ public class Tes extends CustomAbstractSeleniumTest
 		LoginPageImpl.instance(driver).login(tcID, softAssert);
 		
 		LoginPageImpl.instance(driver).logout(tcID, softAssert);
-		System.out.println("Job Testing");
+		System.out.println("Job Testing ");
 		softAssert.assertAll();
+		
+		System.out.println("ava");
 		System.out.println("Finished");
 		
 		
